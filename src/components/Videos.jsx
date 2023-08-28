6eecaaab32a6be08b158ec18b74aca3473255de5
Satyajeet Behera <jagajajpur3@gmail.com>
2023-08-28 20:36:41 +0530

    Update Videos.jsx

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -17,5 +17,16 @@ const Videos = ({ videos, direction }) => {
     </Stack>
   );
 }
-
+/*
+  return (
+    <Stack direction={direction || "row"} flexwrap="wrap" justifycontent="start" alignitems="start" gap={2}>
+      {videos.map((items, idx) =>(
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={items}/>}
+          {item.id.channelId && <ChannelCard channelDetail={items}/>}
+        </Box>
+      ))}
+    </Stack>
+  )
+*/
 export default Videos;
