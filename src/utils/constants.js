628f606283c20c56363dee700266c2d50f848b97
Satyajeet Behera <jagajajpur3@gmail.com>
2023-09-22 11:36:13 +0530

    change

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -33,7 +33,7 @@ export const categories = [
   { name: 'Gym', icon: <FitnessCenterIcon />, },
   { name: 'Crypto', icon: <DeveloperModeIcon />, },
 ];
-
+ 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
 export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
